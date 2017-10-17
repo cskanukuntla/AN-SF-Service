c8bf974174b6d172903f377e05e45ce4873c573b
chandra <cskanukuntla@yahoo.com>
2017-10-17 16:55:35 +0100

    first commit

---
+++ src/main/java/com/an/ansfservices/AnSfServicesApplication.java
@@ -0,0 +1,12 @@
+package com.an.ansfservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnSfServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnSfServicesApplication.class, args);
+	}
+}
